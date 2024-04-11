7afe2ba2aba98748d4b192d28abcbed08c4fa6ed
Roberto Perillo <bperillo@amazon.com>
2024-04-11 08:49:51 -0300

    Fixing race condition possibility

--- cqrs-with-sqs/lambda-codes/order-event-redis-updater.py
+++ cqrs-with-sqs/lambda-codes/order-event-redis-updater.py
@@ -8,11 +8,12 @@ redis_host = os.environ['REDIS_HOST']
 redis_port = os.environ['REDIS_PORT']
 r = redis.Redis(host=redis_host, port=redis_port)
 
+
 def lambda_handler(event, context):
     body = json.loads(event['Records'][0]['body'])
-    if not r.exists(body['messageId']):
-        r.set(body['messageId'], '', ex=10)
+    r.set(body['messageId'], context.aws_request_id, 'NX', 'EX', 300)
 
+    if r.get(body['messageId']) == context.aws_request_id:
         if not r.exists(body['id_client']):
             json_value = {'name': body['name'],
                           'email': body['email'],
